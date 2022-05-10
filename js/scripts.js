4ffab0a1c0da3529bd57d26d06829df37d9f0335
Alexander <knibz@protonmail.com>
2022-05-09 22:13:09 -0700

    add UI logic

--- js/scripts.js
+++ js/scripts.js
@@ -24,3 +24,14 @@ function convertToRoman(num) {
     }
   }
 }
+
+$(document).ready(function() {
+  $("#original-text").submit(function(event){
+    event.preventDefault();
+    const userInput = parseInt($("input#numeral").val());
+
+    const results = convertToRoman (userInput);
+
+    $("#results").text(results);
+  });
+});
